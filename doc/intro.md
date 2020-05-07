3cbd5996f94276464cb906be27bd889ecd2b12b1
Claynon Ellert de Souza <claynon@pm.me>
2020-05-07 08:12:42 +0200

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to aws-sdk-usage-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/aws_sdk_usage_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns aws-sdk-usage-clj.core-test
+  (:require [clojure.test :refer :all]
+            [aws-sdk-usage-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
